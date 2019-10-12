6320c5fb7ddb509642e5f5a3d69df0ac83ddb656
Ercik Agrazal <erick.agrazal@balboadevelopers.com>
2019-10-12 11:44:46 -0500

    Added leer notas.

--- dia4/backend/app.js
+++ dia4/backend/app.js
@@ -3,74 +3,37 @@ const app = express()
 
 const PORT = 3000;
 
-let estudiantes = [
-  {
-    id: 0,
-    nombre: 'Erick Agrazal',
-    edad: '28',
-    profesion: 'Programador'
-  },
-  {
-    id: 1,
-    nombre: 'Rafael Medina',
-    edad: '23',
-    profesion: 'Programador'
-  }
-];
-
 app.use(express.json());
 
-app.get('/estudiantes', function (req, res) {
+let notas = [
+  {
+    nombre: 'Parcial #1',
+    valor: 100,
+  },
+  {
+    nombre: 'Parcial #2',
+    valor: 100,
+  },
+];
+
+app.get('/notas', (req, res) => {
+  let average = 0;
+  notas.forEach((v, i) => {
+    average += v.valor;
+    notas[i] = {...v, id: i};
+  });
   const response = {
-    data: estudiantes,
-    count: estudiantes.length,
+    data: notas,
+    count: notas.length,
+    average: average / notas.length,
   };
   res.json(response);
 });
 
-app.get('/estudiantes/:id', function(req, res){
-  res.json(estudiantes[req.params.id]);
+app.get('/notas/:id', (req, res) => {
+
 });
 
-app.post('/estudiantes', function(req, res){
-  const nuevoEstudiante = { id: estudiantes.length, ...req.body };
-  estudiantes.push(nuevoEstudiante)
-  res.json(nuevoEstudiante);
-});
-
-app.delete('/estudiantes/:id', function(req, res){
-  const { id } = req.params;
-  // for(estudiante of estudiantes){
-  //   if(estudiante.id == id){
-  //     delete estudiantes[id];
-  //     break;
-  //   }
-  // }
-  estudiantes.splice(id, 1);
-  res.json({});
-});
-
-app.put('/estudiantes/:id', function(req, res){
-  // Forma #1
-  // estudiantes = estudiantes.map(function(v, i) {
-  //   if(v.id === req.params.id){ return req.body }
-  //   return v;
-  // });
-  // Forma #2
-  // estudiantes = estudiantes.map((v, i) => {
-  //   if(v.id == req.params.id){
-  //     return {id: v.id, ...req.body};
-  //   }
-  //   else {
-  //     return v;
-  //   }
-  // });
-  // Forma #3
-  estudiantes = estudiantes.map((v, i) => v.id == req.params.id ? {id: v.id, ...req.body} : v);
-  console.log('Estudiantes: ', estudiantes)
-  res.json(req.body);
-})
- 
 app.listen(PORT, function(){
   console.log(`Ejecutando en el puerto: 3000`);
 })
