501b4cf875b62a7bac2e4271e24be9ad01345e3e
Ercik Agrazal <erick.agrazal@balboadevelopers.com>
2019-10-12 13:59:49 -0500

    Added frontend.

--- dia4/frontend/app.js
+++ dia4/frontend/app.js
@@ -5,13 +5,23 @@
         base: 'http://localhost:3000/notas'
       }
     },
+    htmls: {
+      notas: document.getElementById('notas'),
+      formulario: document.getElementById('formulario'),
+      nombre: document.getElementById('nombre'),
+      valor: document.getElementById('valor')
+    },
     init: function(){
       App.initTable();
+      App.bindEvents();
     },
     initTable: function(){
       fetch(App.config.urls.base)
         .then(App.events.onInitTableSuccess)
         .catch(App.events.onInitTableError);
+    },
+    bindEvents: function(){
+      App.htmls.formulario.addEventListener('submit', App.events.onFormularioSubmit);
     },
     events: {
       onInitTableSuccess: function(resp){
@@ -22,12 +32,36 @@
       onInitTableError: function(error){
         console.log('Error: ', error);
       },
-      onJsonParsingSuccess: function(data){
-        console.log(data);
+      onJsonParsingSuccess: function(response){
+        let table = '';
+        response.data.forEach(function(v, i){
+          table += `
+            <tr>
+              <td>${v.nombre}</td>
+              <td>${v.valor}</td>
+            </tr>
+          `
+        });
+        App.htmls.notas.innerHTML = table;
       },
       onJsonParsingError: function(error){
         console.log('Error: ', error);
-      }
+      },
+      onFormularioSubmit: async function(e){
+        e.preventDefault();
+        var body = JSON.stringify({
+          nombre: App.htmls.nombre.value,
+          valor: App.htmls.valor.value,
+        })
+        const response = await fetch(App.config.urls.base, { 
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body,
+        });
+        const data = await response.json();
+        console.log(data);
+        App.initTable();
+      },
     }
   };
   App.init();
