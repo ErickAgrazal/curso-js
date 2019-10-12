afe86daa2cbf5d02873ed722b1380143fbebf3bc
Ercik Agrazal <erick.agrazal@balboadevelopers.com>
2019-10-12 13:35:29 -0500

    Added promises.

--- dia4/frontend/app.js
+++ dia4/frontend/app.js
@@ -1,7 +1,33 @@
 (function(){
   var App = {
+    config: {
+      urls: {
+        base: 'http://localhost:3000/notas'
+      }
+    },
     init: function(){
-      console.log(fetch('http://localhost:3000/notas'));
+      App.initTable();
+    },
+    initTable: function(){
+      fetch(App.config.urls.base)
+        .then(App.events.onInitTableSuccess)
+        .catch(App.events.onInitTableError);
+    },
+    events: {
+      onInitTableSuccess: function(resp){
+        resp.json()
+          .then(App.events.onJsonParsingSuccess)
+          .catch(App.events.onJsonParsingErorr);
+      },
+      onInitTableError: function(error){
+        console.log('Error: ', error);
+      },
+      onJsonParsingSuccess: function(data){
+        console.log(data);
+      },
+      onJsonParsingError: function(error){
+        console.log('Error: ', error);
+      }
     }
   };
   App.init();
